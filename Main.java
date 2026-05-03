bfa96594ffc38fdba67c8bbd85054663fc83a1fb
afrida maisha <maishajarin2002@gmail.com>
2026-05-03 22:24:17 +0600

    merge all feature branches into main branch

--- Main.java
+++ Main.java
@@ -1,11 +1,118 @@
 import java.util.*;
-// class Library {
 
-// }
+class Book {
+    
+    // empty 
+    int id;
+    String title;
+    String author;
+    boolean isIssued;
+
+    // store in book 
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    // print 
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Title: " + title + ", Author: " + author + ", Issued: " + isIssued;
+    }
+}
+
+class Library {
+
+    // ================= store book  =================
+    private final List<Book> books = new ArrayList<>();
+
+    // ================= add book  =================
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Book added successfully!");
+    }
+
+    // ================= view Books =================
+    public void viewBooks() {
+        System.out.println("Viewing all books...");
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+            return;
+        }
+
+        for (Book b : books) {
+            System.out.println(b);
+        }
+    }
+
+    // ================= search book =================
+    public void searchBook(int id) {
+        System.out.println("Searching book...");
+        for (Book b : books) {
+            if (b.id == id) {
+                System.out.println("Found: " + b);
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+
+    // ================= Issue Book =================
+    public void issueBook(int id) {
+        System.out.println("Issuing book...");
+        for (Book b : books) {
+            if (b.id == id) {
+                if (!b.isIssued) {
+                    b.isIssued = true;
+                    System.out.println("Book issued successfully!");
+                } else {
+                    System.out.println("Book already issued!");
+                }
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+
+    // ================= return Book =================
+    public void returnBook(int id) {
+        System.out.println("Returning book...");
+        for (Book b : books) {
+            if (b.id == id) {
+                if (b.isIssued) {
+                    b.isIssued = false;
+                    System.out.println("Book returned successfully!");
+                } else {
+                    System.out.println("Book was not issued!");
+                }
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+
+    // =================delete Book =================
+    public void deleteBook(int id) {
+        System.out.println("Deleting book...");
+        Iterator<Book> it = books.iterator();
+        while (it.hasNext()) {
+            Book b = it.next();
+            if (b.id == id) {
+                it.remove();
+                System.out.println("Book deleted successfully!");
+                return;
+            }
+        }
+        System.out.println("Book not found!");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       // Library library = new Library();
+        Library library = new Library();
 
         while (true) {
             System.out.println("\n===== Library Management System =====");
@@ -30,31 +137,31 @@ public class Main {
                     System.out.print("Enter Author: ");
                     String author = sc.nextLine();
 
-                    // library.addBook(new Book(id1, title, author));
+                    library.addBook(new Book(id1, title, author));
                     break;
 
                 case 2:
-                    //library.viewBooks();
+                    library.viewBooks();
                     break;
 
                 case 3:
                     System.out.print("Enter Book ID to search: ");
-                    // library.searchBook(sc.nextInt());
+                    library.searchBook(sc.nextInt());
                     break;
 
                 case 4:
                     System.out.print("Enter Book ID to issue: ");
-                    // library.issueBook(sc.nextInt());
+                    library.issueBook(sc.nextInt());
                     break;
 
                 case 5:
                     System.out.print("Enter Book ID to return: ");
-                    //library.returnBook(sc.nextInt());
+                    library.returnBook(sc.nextInt());
                     break;
 
                 case 6:
                     System.out.print("Enter Book ID to delete: ");
-                    // library.deleteBook(sc.nextInt());
+                    library.deleteBook(sc.nextInt());
                     break;
 
                 case 7:
